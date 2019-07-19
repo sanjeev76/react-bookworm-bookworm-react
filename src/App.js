b7d2fc26a7e17a6dcda1722eaab14e13224fe83e
sanjeev <sanjeev.itcareer@live.com>
2019-07-19 19:12:38 +0530

    M

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
-import './App.css';
+
 
 const App = () => (
 	<div className="ui container">
---
+++ src/components/messages/InLineError.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "proptypes";
+
+const InLineError = ({ text }) => <span style={{ color: "ae5856" }}>{text}</span>;
+
+InLineError.propTypes = {
+    text: PropTypes.string.isRequired
+
+}
+
+export default InLineError;
+
