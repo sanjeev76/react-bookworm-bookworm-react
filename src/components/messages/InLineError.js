107ece15dbdb2da0e0e589ae5c38f5aa296e1dd9
sanjeev <sanjeev.itcareer@live.com>
2019-07-19 19:16:38 +0530

    m

--- src/components/messages/InLineError.js
+++ src/components/messages/InLineError.js
@@ -6,7 +6,7 @@ const InLineError = ({ text }) => <span style={{ color: "ae5856" }}>{text}</span
 InLineError.propTypes = {
     text: PropTypes.string.isRequired
 
-}
+};
 
 export default InLineError;
 
