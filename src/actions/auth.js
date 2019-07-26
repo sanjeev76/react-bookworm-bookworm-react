ab5c933a42d22aea61d151c74b3a2e5249b9cbd9
sanjeev <sanjeev.itcareer@live.com>
2019-07-26 12:53:34 +0530

    Login/Logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOGGED_IN } from "../types";
+import { USER_LOGGED_IN, USER_LOGGED_OUT } from "../types";
 import api from "../api";
 
 export const userLoggedIn = user => ({
@@ -6,9 +6,23 @@ export const userLoggedIn = user => ({
     user
 });
 
+
+export const userLoggedOut = () => ({
+    type: USER_LOGGED_OUT
+});
+
 // thunk action is just a function that returns another function 
 // will made API request, all our api request will store in one api oject
 // it will return the promice and the data that return the serer
 // dispatche redux ation that will change reducers in redux store
 export const login = credentials => dispatch =>
- api.user.login(credentials).then(user => dispatch(userLoggedIn(user)));
+ api.user.login(credentials).then(user => { 
+     localStorage.bookwormJWT = user.token; //save token to local storage when we login
+     dispatch(userLoggedIn(user))
+    });
+
+    //logout thunk action
+    export const logout = credentials => dispatch => {
+     localStorage.removeItem("bookwormJWT"); 
+     dispatch(userLoggedOut());
+    };
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,9 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-const HomePage = () => (
+// to make the page logout, in redux state we have user email and token
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import * as actions from "../../actions/auth"
+import { CommentActions } from "semantic-ui-react";
+
+
+const HomePage = ({ isAuthenticated, logout }) => (
     <div>
         <h1>Home Page</h1>
-        <Link to="/Login">Login</Link>
+        {isAuthenticated ? <button onClick={() => logout()}>Logout</button> : <Link to="/Login">Login</Link>}
     </div>
     );
-    export default HomePage;
+
+    //Add prop types to home page 
+    HomePage.propTypes = {
+        isAuthenticated: PropTypes.bool.isRequired,
+        logout: PropTypes.func.isRequired
+            
+        }
+    
+
+    //defind a function that returns a props that will be available on home page
+    
+    function mapStateToProps(state) {
+        return { 
+            isAuthenticated: !!state.user.token
+
+        }
+    }
+
+    export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
